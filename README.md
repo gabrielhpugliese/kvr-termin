4370a9de9f1c85af1d2a5c0086355899c4233a37
Gabriel Pugliese <gabriel.pugliese@ifena.de>
2020-03-04 15:56:32 +0100

    Make the appointment automatically

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # kvr-termin
 
+I tried only for Führerscheinstelle page: https://www22.muenchen.de/view-fs/termin/index.php
+
+## Quick start
+
 1. `yarn install`
-2. `yarn start`
+2. Change the function `check_dates_curl` with your curl copied from Chrome (go to page above > inspector > network > reload page > right click on index.php > copy > copy as cURL)
+3. Run `PHPSESSID="GET_THIS_FROM_THE_CURL_URL" TERMIN_ID="GET_THIS_FROM_INSPECTING_ELEMENT" SALUTATION="Herr" NAME="Hahn Thun" BIRTHDAY="01.01.1990" EMAIL="got@it.com" MONTHS_IN_ADVANCE=1 yarn start`
 
-Check the code to change variables like:
+## Setup
 
-1. URL of the termin
-2. The key of the "jsonAppointments" global object
-3. Time interval of when it runs
-4. How many days/months in advance
+For PHPSESSID you get it from the curl you copied from Chrome (Get from PHPSESSID={...here...}).
+
+For TERMIN_ID you must choose your termin, click on Weiter/Next and then on the next page, inspect it and find the variable `jsonAppointments` inside the `<head>` tag. For example, if you choose the `Umschreibung eines ausländischen Führerscheins > Umschreibung eines ausländischen Führerscheins (kein EU/EWR-Führerschein) beantragen`, the value will be `Termin FS Allgemeinschalter_G`
+
+For MONTHS_IN_ADVANCE choose how many months in advance it should check
+
+The other env vars are your personal info.
+
+If a date is found, a sound will beep.
+
+Good luck.
+
+I am not responsible for any mistake made by anyone using this script. It is meant for learning purposes.
--- run.js
+++ run.js
@@ -4,9 +4,32 @@ const cheerio = require('cheerio');
 const player = require('play-sound')((opts = {}));
 const moment = require('moment');
 
-const curl = () => {
-  const args = `PASTE_THE_CURL_FROM_CHROME_HERE`;
+const {
+  PHPSESSID,
+  TERMIN_ID,
+  SALUTATION,
+  NAME,
+  BIRTHDAY,
+  EMAIL,
+  MONTHS_IN_ADVANCE,
+} = process.env;
 
+const check_dates_curl = () =>
+  `PASTE_YOUR_CURL_HERE`;
+
+const pick_date_curl = ({ date, time }) =>
+  `curl 'https://www22.muenchen.de/view-fs/termin/index.php?' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: https://www22.muenchen.de' -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'Sec-Fetch-User: ?1' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: navigate' -H 'Referer: https://www22.muenchen.de/view-fs/termin/index.php?' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' -H 'Cookie: PHPSESSID=${PHPSESSID}' --data 'step=WEB_APPOINT_NEW_APPOINT&APPOINT=${encodeURIComponent(
+    TERMIN_ID,
+  )}___${date}___${time.replace(':', '%3A')}' --compressed`;
+
+const finish_termin_curl = () =>
+  `curl 'https://www22.muenchen.de/view-fs/termin/index.php?' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: https://www22.muenchen.de' -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'Sec-Fetch-User: ?1' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: navigate' -H 'Referer: https://www22.muenchen.de/view-fs/termin/index.php?' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' -H 'Cookie: PHPSESSID=${PHPSESSID}' --data 'step=WEB_APPOINT_SAVE_APPOINT&CONTACT%5Bsalutation%5D=${SALUTATION}&CONTACT%5Bname%5D=${encodeURIComponent(
+    NAME,
+  )}&CONTACT%5Bbirthday%5D=${BIRTHDAY}&CONTACT%5Bemail%5D=${encodeURIComponent(
+    EMAIL,
+  )}&CONTACT%5Bprivacy%5D=1' --compressed`;
+
+const curl = (args) => {
   return new Promise((resolve, reject) => {
     exec(args, (err, stdout, stderr) => {
       if (err) {
@@ -21,7 +44,7 @@ const curl = () => {
 
 const run = async () => {
   console.log('Running', moment().toISOString());
-  const page = await curl();
+  const page = await curl(check_dates_curl());
   const $ = cheerio.load(page);
   const script = $('script').get(3);
 
@@ -31,24 +54,46 @@ const run = async () => {
       .split(/'/)[1],
   );
 
-  const appoints = jsonAppointments['Termin FS Allgemeinschalter_G'].appoints;
-
+  const appoints = jsonAppointments[TERMIN_ID].appoints;
+  let found;
   Object.keys(appoints).forEach((key) => {
+    if (found) {
+      return;
+    }
+
     const date = moment(key);
     const wish = moment()
-      .add(1, 'month')
+      .add(MONTHS_IN_ADVANCE, 'month')
       .endOf('day');
 
     if (date.isBefore(wish) && appoints[key].length !== 0) {
       player.play('beep-01a.mp3');
-      console.log(key, appoints[key]);
+      found = {
+        date: key,
+        time: appoints[key][0],
+      };
+      console.log('found', key, appoints[key][0]);
     }
   });
+
+  if (!found) {
+    console.log('Did not find dates');
+    return;
+  }
+
+  await curl(pick_date_curl(found));
+  await curl(finish_termin_curl());
+
+  console.log('got an appointment!', found.date, found.time);
+  process.exit();
 };
 
 run();
 setInterval(async () => {
   try {
     await run();
-  } catch (err) {}
+  } catch (err) {
+    console.log('some error happened, trying again');
+    console.error(err);
+  }
 }, 30 * 1000);
